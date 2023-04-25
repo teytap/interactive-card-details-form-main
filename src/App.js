19ea43bf024bb0c856d4b9f64d0bb21adeaca9e3
Mehtap Tataroğlu <teytap@gmail.com>
2023-04-25 18:47:50 +0300

    updated form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 const confirmEl = document.getElementById("confirm");
+const continueEl = document.querySelector(".continue");
+
 const nameEl = document.getElementById("name");
 const cardNumber = document.getElementById("card-number");
+const year = document.getElementById("year");
+const month = document.getElementById("month");
+const cvc = document.getElementById("cvc");
+
 const form = document.querySelector("form");
 const thankYou = document.getElementById("thank-you-state");
 
 confirmEl.addEventListener("click", () => {
   thankYou.style.display = "block";
   form.style.display = "none";
+  document.getElementById("left-name").innerHTML = nameEl.value;
+  document.querySelector(".year-container").innerHTML = year.value;
+  document.querySelector(".month-container").innerHTML = month.value;
+  document.querySelector(".card-back-side").innerHTML = cvc.value;
+  document.querySelector(".card-number-div").innerHTML = format(
+    cardNumber.value
+  );
+});
+continueEl.addEventListener("click", () => {
+  thankYou.style.display = "none";
+  form.style.display = "block";
 });
 nameEl.addEventListener("change", () => {
   document.getElementById("left-name").innerHTML = nameEl.value;
 });
+cardNumber.addEventListener("change", () => {
+  document.querySelector(".card-number-div").innerHTML = format(
+    cardNumber.value
+  );
+});
+// splits credit card numbers
+function format(num) {
+  return num.toString().replace(/\d{4}(?=.)/g, "$& ");
+}
