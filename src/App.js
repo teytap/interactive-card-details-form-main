593b7a143bf55f48f988afdb7f8616e0fcda5f81
Mehtap Tataroğlu <teytap@gmail.com>
2023-04-25 23:14:54 +0300

    updated function and css

--- src/App.js
+++ src/App.js
@@ -10,29 +10,38 @@ const cvc = document.getElementById("cvc");
 const form = document.querySelector("form");
 const thankYou = document.getElementById("thank-you-state");
 
-confirmEl.addEventListener("click", () => {
-  thankYou.style.display = "block";
-  form.style.display = "none";
-  document.getElementById("left-name").innerHTML = nameEl.value;
-  document.querySelector(".year-container").innerHTML = year.value;
-  document.querySelector(".month-container").innerHTML = month.value;
-  document.querySelector(".card-back-side").innerHTML = cvc.value;
-  document.querySelector(".card-number-div").innerHTML = format(
-    cardNumber.value
-  );
+confirmEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!nameEl.value) {
+    nameEl.classList.add("error");
+    nameEl.parentElement.classList.add("error-message");
+  } else {
+    nameEl.classList.remove("error");
+    nameEl.parentElement.classList.remove("error-message");
+  }
+  if (
+    nameEl.value &&
+    cardNumber.value.length == 16 &&
+    year.value &&
+    month.value &&
+    cvc.value
+  ) {
+    thankYou.style.display = "block";
+    form.style.display = "none";
+    document.getElementById("left-name").innerHTML = nameEl.value;
+    document.querySelector(".year-container").innerHTML = year.value;
+    document.querySelector(".month-container").innerHTML = month.value;
+    document.querySelector(".cvc-container").innerHTML = cvc.value;
+    document.querySelector(".card-number-div").innerHTML = format(
+      cardNumber.value
+    );
+  }
 });
 continueEl.addEventListener("click", () => {
   thankYou.style.display = "none";
   form.style.display = "block";
 });
-nameEl.addEventListener("change", () => {
-  document.getElementById("left-name").innerHTML = nameEl.value;
-});
-cardNumber.addEventListener("change", () => {
-  document.querySelector(".card-number-div").innerHTML = format(
-    cardNumber.value
-  );
-});
+
 // splits credit card numbers
 function format(num) {
   return num.toString().replace(/\d{4}(?=.)/g, "$& ");
