c564f0ca8034df3f7181838692f6768d7239a30f
Mehtap Tataroğlu <teytap@gmail.com>
2023-04-25 11:38:17 +0300

    add function to form and changed css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 const confirmEl = document.getElementById("confirm");
+const nameEl = document.getElementById("name");
+const cardNumber = document.getElementById("card-number");
+const form = document.querySelector("form");
+const thankYou = document.getElementById("thank-you-state");
 
 confirmEl.addEventListener("click", () => {
-  alert("confirmed");
+  thankYou.style.display = "block";
+  form.style.display = "none";
+});
+nameEl.addEventListener("change", () => {
+  document.getElementById("left-name").innerHTML = nameEl.value;
 });
